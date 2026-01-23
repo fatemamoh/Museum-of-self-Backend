ad806fd04e6e632b3f9cd05ec41572728d8d05c3
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-23 20:01:58 +0300

    adding some routes to the phase ctrl

--- controllers/lifePhases.js
+++ controllers/lifePhases.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const router = express.Router();
 const LifePhase = require('../models/lifePhase');
+
+router.post('/', async (req,res) =>{
+try {
+    req.body.user = req.user._id;
+    const lifePhase = await LifePhase.create(req.body);
+    res.status(201).json(lifePhase);
+} catch (error) {
+    res.status(500).json({err: error.message});
+}
+});
+
+router.get('/', async (req,res) =>{
+try {
+    const lifePhases = (await LifePhase.find({user: req.user._id}))
+    .sort({startDate: -1});
+    res.status(201).json(lifePhases);
+} catch (error) {
+    res.status(500).json({err: error.message});
+}
+});
+
