74d621827f806e726a7605894c91e36c2feb9ca5
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-23 19:45:10 +0300

    creating the life phase schema

--- models/lifePhase.js
+++ models/lifePhase.js
@@ -1,36 +1,36 @@
 const mongoose = require('mongoose');
 
 const lifePhaseSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-        trim:true,
+    title: {
+        type: String,
+        required: true,
+        trim: true,
         maxlength: 100
     },
-    summary:{
-        type:String,
-        required:[true, "A Curator's Statment is required to open this room."],
+    summary: {
+        type: String,
+        required: [true, "A Curator's Statment is required to open this room."],
         minlength: 20
     },
-    startDate:{
-        type:Date,
+    startDate: {
+        type: Date,
         required: true
     },
-    endDate:{
-        type:Date,
-        default:null
+    endDate: {
+        type: Date,
+        default: null
     },
-    theme:{
-        type:String,
-        enum:['gold', 'olive','charcoal','terracotta','slate'],
-        default:'gold'
+    theme: {
+        type: String,
+        enum: ['gold', 'olive', 'charcoal', 'terracotta', 'slate'],
+        default: 'gold'
     },
-    user:{
+    user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
+        ref: 'User',
+        required: true
     }
-}, {timestamps:true}
+}, { timestamps: true }
 );
 
 const LifePhase = mongoose.model('LifePhase', lifePhaseSchema);
