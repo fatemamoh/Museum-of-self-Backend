592b17e86a7361075661b6b2d14a831bb558042c
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-25 15:57:35 +0300

    fixing

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
-const express = require('express');
 
+const express = require('express');
 const app = express();
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
