fe7de8e7d98a41533230cb31f9ad42e23595d158
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-25 10:43:52 +0300

    updating memory schema

--- models/memory.js
+++ models/memory.js
@@ -1,6 +1,7 @@
 const mongoose = require ('mongoose'); 
 
 const memorySchema = new mongoose.Schema({
+
 title:{
     type:String,
     required:[true, 'Every artifact needs a title for the muesum catalog'],
@@ -11,6 +12,12 @@ type:{
     type:String,
     enum:['Text','Image','Video','Audio',,'Link'],
     default:'Text'
+},
+
+size:{
+    type:String,
+    enum:['Small', 'Medium', 'Large'],
+    default:'Medium'
 },
 }, {timestamps: true}); 
 const Memory = mongoose.model('Memory', memorySchema);
