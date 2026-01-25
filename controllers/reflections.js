24ee03bbb25e542bf685496e491a79271b4461ee
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-25 15:02:26 +0300

    reflection create route

--- controllers/reflections.js
+++ controllers/reflections.js
@@ -2,3 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Reflection = require('../models/reflection');
 const Memory = require('../models/memory');
+
+// create reflection
+router.post('/:memoryId', async (req, res)=>{
+try {
+    const memory = await Memory.findById(req.params.memoryId);
+        if (!memory || !memory.user.equals(req.user._id)) {
+            return res.status(403).json({ err: "Cannot reflect on an inaccessible artifact." });
+        }
+
+        req.body.user = req.user._id;
+        req.body.memory = req.params.memoryId;
+        
+        const reflection = await Reflection.create(req.body);
+        res.status(201).json(reflection);
+} catch (error) {
+    res.status(500).json({ err: error.message });
+}
+});
