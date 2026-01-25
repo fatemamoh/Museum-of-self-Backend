f89b7a43b9da9a1bfe5333208a6159d7541f35a4
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-25 14:00:12 +0300

    create route for memory

--- controllers/memories.js
+++ controllers/memories.js
@@ -3,7 +3,20 @@ const router = express.Router();
 const Memory = require('../models/memory');
 const { cloudinary, upload } = require('../config/cloudinary');
 
-
+// create new memory 
+router.post('/', upload.single('file'), async (req, res) => {
+    try {
+        req.body.user = req.user._id;
+        if (req.file) {
+            req.body.contentUrl = req.file.path;
+            req.body.cloudinaryPublicId = req.file.filename;
+        }
+        const memory = await Memory.create(req.body);
+        res.status(201).json(memory);
+    } catch (error) {
+        res.status(500).json({ err: error.message });
+    }
+})
 
 
 
