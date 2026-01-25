4fa3e72266286cfac638946bf8bdc0664a0c6c54
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-25 14:07:21 +0300

    create route for all memories in a phase

--- controllers/memories.js
+++ controllers/memories.js
@@ -16,7 +16,30 @@ router.post('/', upload.single('file'), async (req, res) => {
     } catch (error) {
         res.status(500).json({ err: error.message });
     }
-})
+});
+
+router.get('/phase/:phaseId', async (req, res) => {
+    try {
+        const memories = await Memory.find({
+            phase: req.params.phaseId,
+            user: req.user._id
+        }).sort({ capturedDate: 1 });
+        const curatedMemories = memories.map(memory => {
+            if (memory.unlockDate && new Date() < new Date(memory.unlockDate)) {
+                return {
+                    ...memory._doc,
+                    contentUrl: null,
+                    story: "Locked in a Time Capsule",
+                    isLocked: true
+                };
+            }
+            return memory;
+        });
+        res.status(200).json(curatedMemories);
+    } catch (error) {
+        res.status(500).json({ err: error.message });
+    }
+});
 
 
 
