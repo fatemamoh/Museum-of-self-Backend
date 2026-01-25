71a4f504bf29c3df96274d286276435b708186fb
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-25 15:44:35 +0300

    vault logic

--- middleware/verify-vault.js
+++ middleware/verify-vault.js
@@ -1,16 +1,12 @@
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
 
 const verifyVault = async (req, res, next) => {
     try {
         const providedPin = req.headers['x-master-pin'];
-        if (!providedPin) {
-            return res.status(403).json({ err: "Access Denied: MasterPIN required." });
-        }
-        const user = await User.findById(req.user._id);
-        if (!user) return res.status(404).json({ err: "User not found." });
+        if (!providedPin) return res.status(403).json({ err: "MasterPIN required." });
 
-        const isMatch = await bcrypt.compare(providedPin, user.masterPin);
+        const user = await User.findById(req.user._id);
+        const isMatch = await user.comparePin(providedPin);
 
         if (isMatch) {
             next();
@@ -18,7 +14,7 @@ const verifyVault = async (req, res, next) => {
             res.status(401).json({ err: "Invalid MasterPIN." });
         }
     } catch (error) {
-        res.status(500).json({ err: "Security check failed." });
+        res.status(500).json({ err: error.message });
     }
 };
 
